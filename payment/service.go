bf5b5b2f10bd192e5ffbd50e6ce34ccc86e21ce1
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-09-05 17:20:19 -0300

    procurando url correta assim que a requisição der errado

--- payment/service.go
+++ payment/service.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/bernardoazevedo/rinha-de-backend-2025/health"
 	"github.com/bernardoazevedo/rinha-de-backend-2025/key"
 )
 
@@ -17,21 +18,20 @@ func postPayment(payment Payment) (string, error) {
 		return "", errors.New("error parsing payment")
 	}
 
-	paymentDefaultUrl := os.Getenv("PAYMENT_DEFAULT_URL")
-	paymentFallbackUrl := os.Getenv("PAYMENT_FALLBACK_URL")
 	url, _ := key.Get("url")
 
 	postBody := bytes.NewBuffer(paymentJson)
-	response, err := http.Post(url+"/payments", "application/json", postBody)
-	if err != nil {
-		
-		if url == paymentDefaultUrl {
-			url = paymentFallbackUrl
-			key.Set("url", url)
+
+	var response *http.Response
+	for {
+		response, err = http.Post(url+"/payments", "application/json", postBody)
+		if err != nil {
+			url, _ = health.CheckSetReturnUrl()			
+		} else {
+			break //success
 		}
-		return "", errors.New("error during request")
+		defer response.Body.Close()
 	}
-	defer response.Body.Close()
 
 	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
